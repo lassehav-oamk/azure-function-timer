0ce0b6056d82388eecd343f7e243cd6f29972d27
Lasse Haverinen <lasse.haverinen@gmail.com>
2023-11-27 12:54:47 +0200

    Added axios call to APP_URL

--- src/functions/randomQuoteTimerFunction.js
+++ src/functions/randomQuoteTimerFunction.js
@@ -1,8 +1,21 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
+const axios = require("axios");
 
-app.timer('randomQuoteTimerFunction', {
-    schedule: '0 */1 * * * *',
-    handler: (myTimer, context) => {
-        context.log('Timer function processed request.');
+app.timer("randomQuoteTimerFunction", {
+  schedule: "0 */1 * * * *",
+  handler: async (myTimer, context) => {
+    context.log("Timer function processed request.");
+    try {
+      const response = await axios.get(process.env.APP_URL);
+      context.res = {
+        status: response.status,
+        body: response.data,
+      };
+    } catch (error) {
+      context.res = {
+        status: error.response.status || 500,
+        body: error.message,
+      };
     }
+  },
 });
